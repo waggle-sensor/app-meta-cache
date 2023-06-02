69457bfa67ed48df59786151d7a9cc1eaac2d988
Yongho Kim <ironyonghokim@gmail.com>
2023-06-02 14:35:07 -0500

    Added retry mechanism

--- cmd/update_app_cache/cmd/set.go
+++ cmd/update_app_cache/cmd/set.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/spf13/cobra"
 	kube "github.com/waggle-sensor/app-meta-cache/pkg/kube_client"
@@ -12,11 +13,13 @@ import (
 var (
 	nodeName   string
 	kubeconfig string
+	retryCount int
 )
 
 func init() {
 	setCmd.Flags().StringVar(&kubeconfig, "kubeconfig", getenv("KUBECONFIG", ""), "path to the kubeconfig file")
 	setCmd.Flags().StringVar(&nodeName, "nodename", getenv("HOST", ""), "Kubernetes node name to get node labels")
+	setCmd.Flags().IntVar(&retryCount, "retry-count", 0, "Retry count when fails")
 	rootCmd.AddCommand(setCmd)
 }
 
@@ -32,29 +35,44 @@ var setCmd = &cobra.Command{
 		if err != nil {
 			return fmt.Errorf("failed to parse value %q: %s", value, err.Error())
 		}
-		if kubeClient, err := kube.NewKubeClient(kubeconfig); err != nil {
+		kubeClient, err := kube.NewKubeClient(kubeconfig)
+		if err != nil {
 			return fmt.Errorf("failed to get Kubernetes client: %s", err.Error())
-		} else {
-			if nodeName == "" {
-				return fmt.Errorf("no Kubernetes node name is given")
+		}
+		if nodeName == "" {
+			return fmt.Errorf("no Kubernetes node name is given")
+		}
+		for i := 0; i <= retryCount; i++ {
+			if labels, err := kubeClient.GetNodeLabels(nodeName); err != nil {
+				fmt.Printf("failed to get labels from node %q: %s", nodeName, err.Error())
+				fmt.Printf("%d/%d retrying in 2 seconds", i, retryCount)
+				time.Sleep(2)
+				continue
 			} else {
-				if labels, err := kubeClient.GetNodeLabels(nodeName); err != nil {
-					return fmt.Errorf("failed to get labels from node %q: %s", nodeName, err.Error())
+				if zone, found := labels["zone"]; found {
+					meta["zone"] = zone
+					fmt.Printf("added zone %s\n", zone)
 				} else {
-					if zone, found := labels["zone"]; found {
-						meta["zone"] = zone
-						fmt.Printf("added zone %s\n", zone)
-					} else {
-						return fmt.Errorf("failed to find zone label from node %q: %v", nodeName, labels)
-					}
+					return fmt.Errorf("failed to find zone label from node %q: %v", nodeName, labels)
 				}
 			}
 		}
-		if updatedMeta, err := json.Marshal(meta); err != nil {
+
+		updatedMeta, err := json.Marshal(meta)
+		if err != nil {
 			return fmt.Errorf("failed to serialize app meta %v: %s", meta, err.Error())
-		} else {
-			redisClient := redis.NewRedisClient(WESAppCacheHost)
-			return redisClient.Set(key, string(updatedMeta))
 		}
+		redisClient := redis.NewRedisClient(WESAppCacheHost)
+		for i := 0; i <= retryCount; i++ {
+			if err = redisClient.Set(key, string(updatedMeta)); err != nil {
+				fmt.Printf("failed to set meta to %s: %s", WESAppCacheHost, err.Error())
+				fmt.Printf("%d/%d retrying in 2 seconds", i, retryCount)
+				time.Sleep(2)
+				continue
+			} else {
+				return nil
+			}
+		}
+		return fmt.Errorf("failed to complete the task: %s", err.Error())
 	},
 }
